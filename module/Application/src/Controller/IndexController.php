1225c424ac86d257bf2cb34899235ab95c816293
Ali Rasheed <arshaikh_17@hotmail.com>
2019-04-10 12:28:32 +0100

    Fixed formatting of index controller.

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -1,19 +1,20 @@
 <?php
-/**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Application\Service\XmlManager;
 
-class IndexController extends AbstractActionController
-{
-    public function indexAction()
-    {
-        return new ViewModel();
-    }
+class IndexController extends AbstractActionController {
+	
+	/**
+	 * Displays index view.
+	 */
+	public function indexAction() {
+		
+		return new ViewModel();
+		
+	}
+	
 }
