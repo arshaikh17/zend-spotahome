6f357065566eab03a086ea2aaf0096dd00fa5a41
Ali Rasheed <arshaikh_17@hotmail.com>
2019-04-10 11:03:46 +0100

    Added service manager for xml.

---
+++ module/Application/src/Service/XmlManager.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This class handles the fetching and parsing of xml data.
+ */
+
+namespace Application\Service;
+
+class XmlManager {
+	
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		
+	}
+	
+}
