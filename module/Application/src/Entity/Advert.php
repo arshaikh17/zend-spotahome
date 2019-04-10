c8e84e19359ab0e474f2f7fd3685c85b379cb99b
Ali Rasheed <arshaikh_17@hotmail.com>
2019-04-10 11:48:03 +0100

    Added attributes and methods for set/get in model.

--- module/Application/src/Entity/Advert.php
+++ module/Application/src/Entity/Advert.php
@@ -7,4 +7,120 @@
 namespace Application\Entity;
 
 class Advert {
+	
+	/**
+	 * Advert ID
+	 */
+	private $id;
+	
+	/**
+	 * Advert Title
+	 */
+	private $title;
+	
+	/**
+	 * Advert Link
+	 */
+	private $url;
+	
+	/**
+	 * Advert City
+	 */
+	private $city;
+	
+	/**
+	 * Advert Picture
+	 */
+	private $picture;
+	
+	/**
+	 * Gets the ID
+	 */
+	public function getID () {
+		
+		return $this->id;
+		
+	}
+	
+	/**
+	 * Sets the ID
+	 */
+	public function setID ($id) {
+		
+		$this->id						 =	$id;
+		
+	}
+	
+	/**
+	 * Gets the Title
+	 */
+	public function getTitle () {
+		
+		return $this->title;
+		
+	}
+	
+	/**
+	 * Sets the Title
+	 */
+	public function setTitle ($title) {
+		
+		$this->title					 =	$title;
+		
+	}
+	
+	/**
+	 * Gets the URL
+	 */
+	public function getUrl () {
+		
+		return $this->url;
+		
+	}
+	
+	/**
+	 * Sets the URL
+	 */
+	public function setUrl ($url) {
+		
+		$this->url						 =	$url;
+		
+	}
+	
+	/**
+	 * Gets the City
+	 */
+	public function getCity () {
+		
+		return $this->city;
+		
+	}
+	
+	/**
+	 * Sets the City
+	 */
+	public function setCity ($city) {
+		
+		$this->city						 =	$city;
+		
+	}
+	
+	/**
+	 * Gets the Picture
+	 */
+	public function getPicture () {
+		
+		return $this->picture;
+		
+	}
+	
+	/**
+	 * Sets the Picture
+	 */
+	public function setPicture ($picture) {
+		
+		$this->picture					 =	$picture;
+		
+	}
+	
 }
