359d3b9001b5f1a80e43226e3ec7443d601f7f8a
Ali Rasheed <arshaikh_17@hotmail.com>
2019-04-10 12:42:17 +0100

    Added constructor and manager to call xml methods.

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -8,12 +8,30 @@ use Application\Service\XmlManager;
 
 class IndexController extends AbstractActionController {
 	
+	private $xmlManager;
+	
+	/**
+	 * Construction
+	 */
+	public function __construct () {
+		
+		$this->xmlManager				 =	new XmlManager();
+		
+	}
+	
 	/**
 	 * Displays index view.
 	 */
 	public function indexAction() {
 		
-		return new ViewModel();
+		$adverts						 =	[];
+		$data							 =	$this->xmlManager->fetchData();
+		
+		if ($data["success"]) $adverts	 =	$this->xmlManager->mapEntities($data["data"]->ad);
+		
+		return [
+			"adverts"					 =>	$adverts
+		];
 		
 	}
 	
